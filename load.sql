7de06db95904b71233cf271f2beea7f1a2c25852
Carlos Aser <cplantijn@gmail.com>
2017-05-02 19:22:43 -0400

    Nuevos inserts y se agrega extension .sql a los archivos

---
+++ load.sql
@@ -0,0 +1,16 @@
+INSERT INTO bebedor VALUES ('12345678','Juan Perez');
+INSERT INTO bebedor VALUES ('11223344','Maria Gomez');
+INSERT INTO bebedor VALUES ('11122333','Antonio Torres');
+INSERT INTO bebedor VALUES ('87654321','Ana Suarez');
+
+INSERT INTO bebida VALUES ('109','Merengada de oreo');
+INSERT INTO bebida VALUES ('110','McFlurry pirulin');
+
+INSERT INTO fuente_soda VALUES ('1','El punto');
+INSERT INTO fuente_soda VALUES ('2','La fruteria');
+
+INSERT INTO gusta VALUES ('87654321','110');
+INSERT INTO gusta VALUES ('11223344','109');
+
+INSERT INTO frecuenta VALUES ('11223344','1');
+INSERT INTO frecuenta VALUES ('87654321','2')
