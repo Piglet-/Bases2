56453dc84ddde1436a3db16cdc1fc811d35966cf
Piglet- <cferreira6594@gmail.com>
2017-05-04 22:36:58 -0400

    Se agregaron unos load para probar el tercer query, siento que se puede mejorar mucho ese query pero funciona

--- load.sql
+++ load.sql
@@ -5,12 +5,22 @@ INSERT INTO bebedor VALUES ('87654321','Ana Suarez');
 
 INSERT INTO bebida VALUES ('109','Merengada de oreo');
 INSERT INTO bebida VALUES ('110','McFlurry pirulin');
+INSERT INTO bebida VALUES ('111','Malta');
 
 INSERT INTO fuente_soda VALUES ('1','El punto');
 INSERT INTO fuente_soda VALUES ('2','La fruteria');
 
 INSERT INTO gusta VALUES ('87654321','110');
+INSERT INTO gusta VALUES ('87654321','111');
+
 INSERT INTO gusta VALUES ('11223344','109');
+INSERT INTO gusta VALUES ('11223344','111');
+
 
 INSERT INTO frecuenta VALUES ('11223344','1');
-INSERT INTO frecuenta VALUES ('87654321','2')
+INSERT INTO frecuenta VALUES ('87654321','2');
+
+INSERT INTO vende VALUES ('1','109','500');
+INSERT INTO vende VALUES ('1','111','100');
+INSERT INTO vende VALUES ('2','109','500');
+
--- query.sql
+++ query.sql
@@ -1,3 +1,19 @@
-SELECT gusta.ci, frecuenta.ci
-FROM GUSTA, FRECUENTA
-WHERE gusta.ci = frecuenta.ci 
+-- (Query 1) 3. Los bebedores que les gusta al menos una bebida y que 
+-- frecuentan al menos una fuente de soda. 
+SELECT B.CI, B.NOMBRE
+FROM GUSTA G, FRECUENTA F, BEBEDOR B
+WHERE   G.ci = F.ci AND
+        G.ci = B.ci;
+
+-- (Query 3) 27. Las bebidas que se sirven en las fuentes de soda que son 
+-- frecuentadas por las personas que les gusta la malta
+
+SELECT v.CodBeb, b2.NombreBeb
+FROM gusta g, frecuenta f, bebida b, bebedor be, vende v, bebida b2
+WHERE   b.NombreBeb = 'Malta' AND
+        b.CodBeb = g.CodBeb AND
+        g.ci = be.ci AND
+        be.ci = f.ci AND
+        f.CodFS = v.CodFS AND
+        v.CodBeb = b2.CodBeb
+
